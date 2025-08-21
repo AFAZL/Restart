1ddb1b4662d3d9416e1768fda0c4dac7da942166
Afzal Siddiquie <afzalsiddiquie63@gmail.com>
2025-08-21 22:14:05 +0530

    Initial commit

---
+++ CBT.java
@@ -0,0 +1,105 @@
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+
+class CBT {
+    Node root;
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BT {
+        static int i = -1;
+
+        public static Node buildTree(int[] nodes) {
+            i++;
+            if (nodes[i] == -1) {
+                return null;
+            }
+            Node nn = new Node(nodes[i]);
+            nn.left = buildTree(nodes);
+            nn.right = buildTree(nodes);
+
+            return nn;
+        }
+    }
+
+    public static void preOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    public static void postOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void levelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while (!q.isEmpty()) {
+            Node c = q.remove();
+            if (c == null) {
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(c.data + " ");
+                if (c.left != null) {
+                    q.add(c.left);
+                }
+                if (c.right != null) {
+                    q.add(c.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        Node ans = BT.buildTree(nodes);
+        System.out.println(ans.data);
+        preOrder(ans);
+        System.out.println("");
+        inOrder(ans);
+        System.out.println("");
+        postOrder(ans);
+        System.out.println("");
+        levelOrder(ans);
+    }
+}
