3cb2e9fb21d1d8f19cf91e781342fefa9fbfcd2c
Afzal Siddiquie <afzalsiddiquie63@gmail.com>
2025-08-27 21:26:02 +0530

    updated files

--- BST.java
+++ BST.java
@@ -1,4 +1,18 @@
+import java.util.*;
+
 public class BST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Lets Move to Advance :-> ");
     }
