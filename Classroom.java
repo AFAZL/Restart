aa5943cc528c7896af3a248ced8c872a844a72fc
Afzal Siddiquie <afzalsiddiquie63@gmail.com>
2025-09-22 22:21:39 +0530

    updated files

---
+++ Classroom.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Classroom {
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void main(String args[]) {
+        int v = 4;
+        ArrayList<Edge> graph[] = new ArrayList[v];
+
+    }
+}
